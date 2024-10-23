139a32be2b3fc14ebada7883f4496d71149d1d20
Rumman93 <ahmmedjubayer2015@gmail.com>
2024-10-23 16:51:06 +0600

    singleton initialized

---
+++ src/singleton/LazySingleton.java
@@ -0,0 +1,30 @@
+package singleton;
+
+/**
+ * Author :  Ahmmed Jubayer Rumman
+ * Created at : 23 Oct 2024
+ */
+
+import java.io.Serializable;
+
+/**
+ * Lazy initialization mean application will create instance when it is requested.
+ * However, this can be used when you have non-thread-safe application. If used in multi threading it might break,
+ * Why? because your getinstance method if invoked by two thread at same time then!!!!
+ * <p>
+ * When to use?  Non thread safe and creating common resource like db connection.
+ */
+public class LazySingleton implements Serializable {
+    private static LazySingleton instance = null;
+
+    private LazySingleton() {
+
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
